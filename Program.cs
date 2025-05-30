07bd595193c9145de7ac3bcae706da81df3bfd7c
Mehreen-Siddique <mehreensiddique50@gmail.com>
2025-05-30 20:02:04 +0500

    resolve the issue with foreign key

--- Program.cs
+++ Program.cs
@@ -30,11 +30,72 @@ if (app.Environment.IsDevelopment())
         config.DocExpansion = "list";
     });
 }
+
+app.MapGet("/", () => "Mehreen  siddique \n22_arid_5114 \nBSCS");
+
+// add the record of designer table.
+
+app.MapGet("/Designer", async (SketchOrganizerDb db) =>
+    await db.Designers.ToListAsync());
+
+
+  // insert the record in the Designer table.
+app.MapPost("Add Data", async (Sketch sketch, SketchOrganizerDb db) =>
+{
+    if (!await db.Designers.AnyAsync(d => d.id == sketch.Designerid))
+    {
+        return Results.BadRequest("Invalid Designerid");
+    }
+    db.Sketches.Add(sketch);
+    await db.SaveChangesAsync();
+    return Results.Created($"/Sketch organizer/{sketch.id}", sketch);
+});
+
+
+
+
+// put function  to update the record of table sketch.
+
+app.MapPut("put/update data", async (int id, Designer inputdesigner, SketchOrganizerDb db) =>
+{
+    var designer = await db.Designers.FindAsync(id);
+
+    if (designer is null) return Results.NotFound();
+
+    designer.FullName = inputdesigner.FullName;
+
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+// delete the data using the id of designer.
+
+app.MapDelete("Delete Record", async (int id, SketchOrganizerDb db) =>
+{
+    if (await db.Designers.FindAsync(id) is Designer designer)
+    {
+        db.Designers.Remove(designer);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+}); 
+
+
+
+
+
+
+
+
 // see the record of sketch table using get function.
-app.MapGet("/Sketch organizer", async (SketchOrganizerDb db) =>
+ app.MapGet("/Sketch organizer", async (SketchOrganizerDb db) =>
     await db.Sketches.ToListAsync());
 
-  
+
   // insert the record in the sketch table.
     app.MapPost("Add Record", async (Sketch sketch, SketchOrganizerDb db) =>
 {
@@ -64,7 +125,7 @@ app.MapPut("put/update Record", async (int id, Sketch inputsketch, SketchOrganiz
 
 // delete the data using the id of sketch.
 
-app.MapDelete("Delete Record", async (int id, SketchOrganizerDb db) =>
+app.MapDelete("Delete", async (int id, SketchOrganizerDb db) =>
 {
     if (await db.Sketches.FindAsync(id) is Sketch sketch)
     {
@@ -74,9 +135,9 @@ app.MapDelete("Delete Record", async (int id, SketchOrganizerDb db) =>
     }
 
     return Results.NotFound();
-});
+}); 
 
 
-app.MapGet("/", () => "Mehreen  siddique \n22_arid_5114 \nBSCS");
+
 
 app.Run();
--- steche.cs
+++ steche.cs
@@ -2,9 +2,7 @@ public class Sketch{
     public int id { get; set; }
 
     public int Designerid { get; set; }
-
-    public Designer? Designer{ get; set; }
-
+ 
     public string? sketchTitle { get; set; }
 
     public string? description { get; set; }
