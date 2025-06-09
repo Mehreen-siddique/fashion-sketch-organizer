5f7d2ec37d79423bba0c20becc1ad3e2d2f5fbcc
Mehreen-Siddique <mehreensiddique50@gmail.com>
2025-06-09 13:52:52 +0500

    Refactor SketchOrganizerDb to SketchOrganizerContext and update DbSet properties for better clarity and structure.

--- Sketchorganizerdb.cs
+++ Sketchorganizerdb.cs
@@ -1,22 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 
-public class SketchOrganizerContext : DbContext
+
+class SketchOrganizerDb : DbContext
 {
-    public DbSet<Sketch> Sketches { get; set; }
-    public DbSet<Designer> Designers { get; set; }
-      public DbSet<Tag> Tags { get; set; }
-
-    public string DbPath { get; }
-
-    public SketchOrganizerContext()
-    {
-        var folder = Environment.SpecialFolder.LocalApplicationData;
-        var path = Environment.GetFolderPath(folder);
-        DbPath = Path.Join(path, "SketchOrganizer.db");
-    }
-
-    // The following configures EF to create a Sqlite database file in the
-    // special "local" folder for your platform.
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite($"Data Source={DbPath}");
+    public SketchOrganizerDb(DbContextOptions<SketchOrganizerDb> options)
+     : base(options) { }
+    public DbSet<Sketch> Sketches => Set<Sketch>();
+    public DbSet<Designer> Designers => Set<Designer>();
+     public DbSet<Tag> Tags => Set<Tag>();
 }
